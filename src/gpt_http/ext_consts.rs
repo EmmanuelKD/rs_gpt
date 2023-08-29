9be0e0f9aecf05e7bc08286e8d5b919ca8fc4924
Emmanuel  Danquah <40697413+EmmanuelKD@users.noreply.github.com>
2023-08-29 21:48:34 +0000

    Delete ext_consts.rs

--- src/gpt_http/ext_consts.rs
+++
@@ -1,6 +0,0 @@
-// REF: https://platform.openai.com/docs/api-reference/chat
-
-pub const API_KEY:&str="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";// key alart, dont worry it has 0 
-pub const GPT_API_URL:&str="https://api.openai.com/v1/chat/completions";
-
- 
