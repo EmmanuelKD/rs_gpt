e4f7f7da089a0e8c3c56c6dcdee56f0f398fc0dc
kemmanuel662@gmail.com <EmmanuelKD>
2023-08-29 21:44:58 +0000

    removed my key

---
+++ src/gpt_http/example.ext_consts.rs
@@ -0,0 +1,6 @@
+// REF: https://platform.openai.com/docs/api-reference/chat
+
+pub const API_KEY:&str="";// key alart, dont worry it has 0 
+pub const GPT_API_URL:&str="https://api.openai.com/v1/chat/completions";
+
+ 
--- src/main.rs
+++ src/main.rs
@@ -12,47 +12,6 @@ mod gpt_http {
     pub mod ext_consts;
 }
 
-// #[tokio::main]
-// fn main() {
-// let mut option: i8 = -1;
-// let mut input_line = String::new();
-
-// let mut builder = Builder::default();
-// builder.append(
-//     "\n\nHello, welcome to cli chat assistant, these are your commands, \n".blue().to_string()
-// );
-// builder.append("1. ".red().to_string());
-// builder.append("for chat\n".blue().to_string());
-// builder.append("2. ".red().to_string());
-// builder.append("to see history chat history and \n".blue().to_string());
-// builder.append("0. ".red().to_string());
-// builder.append("to exit the app\n\n".blue().to_string());
-
-// println!("{}", builder.string().unwrap());
-
-//     while option != 0 {
-//         if option != 1 {
-//             // if option == 1 meaning we are still charting
-//             print!("please enter option: ");
-//             std::io::stdout().flush().unwrap();
-//         }
-
-//         std::io::stdin().read_line(&mut input_line).unwrap();
-//         option = input_line.trim().parse().expect("Input not an integer");
-//         match option {
-//             1 => {
-//                 option = chat(option);
-
-//                 std::io::stdout().flush().unwrap();
-//                 println!("{}", "say something >> ".green().to_string());
-//             }
-//             2 => see_chat_history(),
-//             0 => exit_app(),
-//             _ => print!("please enter correct option: "),
-//         }
-//     }
-// }
-
 fn main() {
     let mut option: i8 = -1;
     let mut input_line = String::new();
